13483ba8aa5fe4a345f8d40e8ec30d5bb6330c2d
Toteya <111172685+Toteya@users.noreply.github.com>
2025-01-30 14:09:53 +0200

    removed debug=True option

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,4 +16,4 @@ cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 if __name__ == '__main__':
     host = '0.0.0.0'
     port = '5000'
-    app.run(host=host, port=port, threaded=True, debug=True)
+    app.run(host=host, port=port, threaded=True)
